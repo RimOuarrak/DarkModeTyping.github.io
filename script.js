7f802fcc99ac9b7a7169a088a22bfefe88bc8f4e
Boutzougarte walid <69628601+walidbtz7@users.noreply.github.com>
2021-11-15 00:39:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,42 +7,39 @@ var checkbox = document.querySelector('input[name=theme]');
         checkbox.addEventListener('change', function() {
             if(this.checked) {
                 trans()
-                document.documentElement.setAttribute('data-theme', 'dark')
-            } else {
+                document.documentElement.setAttribute('data-theme', 'dark');            } else {
                 trans()
-                document.documentElement.setAttribute('data-theme', 'light')
-            }
+                document.documentElement.setAttribute('data-theme', 'light');            }
         })
 
         let trans = () => {
             document.documentElement.classList.add('transition');
             window.setTimeout(() => {
                 document.documentElement.classList.remove('transition')
-            }, 1000)
-        }
+            }, 1000);        }
         ///////////////////////////////////////
         
         
         quoteInputElement.addEventListener('input', () => {
-            const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-            const arrayValue = quoteInputElement.value.split('')
-            let correct = true
+            const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+            const arrayValue = quoteInputElement.value.split('');
+            let correct = true;
             arrayQuote.forEach((characterSpan , index) =>{
-                const character = arrayValue [index]
+                const character = arrayValue [index];
                 if(character== null){
-                    characterSpan.classList.remove('correct')
-                   characterSpan.classList.remove('incorrect')
-                   correct = false
+                    characterSpan.classList.remove('correct');
+                   characterSpan.classList.remove('incorrect');
+                   correct = false;
                 } else if (character === characterSpan.innerText){
-                   characterSpan.classList.add('correct')
-                   characterSpan.classList.remove('incorrect')
+                   characterSpan.classList.add('correct');
+                   characterSpan.classList.remove('incorrect');
                 } else {
-                    characterSpan.classList.remove('correct')
-                   characterSpan.classList.add('incorrect')
-                   correct = false
+                    characterSpan.classList.remove('correct');
+                   characterSpan.classList.add('incorrect');
+                   correct = false;
                 }
             })
-            if(correct) renderNewQuote()
+            if(correct) renderNewQuote();
         })
         function getRandomQuote() {
               return fetch(RANDOM_QUOTE_API_URL)
@@ -51,26 +48,26 @@ var checkbox = document.querySelector('input[name=theme]');
           }
         
           async function renderNewQuote() {
-              const quote = await getRandomQuote()
-              quoteDisplayElement.innerHTML = ''
+              const quote = await getRandomQuote();
+              quoteDisplayElement.innerHTML = '';
               quote.split('').forEach(character => {
-                  const characterSpan = document.createElement('span')
-                  characterSpan.innerText=character
-                  quoteDisplayElement.appendChild(characterSpan)
+                  const characterSpan = document.createElement('span');
+                  characterSpan.innerText=character;
+                  quoteDisplayElement.appendChild(characterSpan);
               });
-              quoteInputElement.value = null
-              startTimer()
+              quoteInputElement.value = null;
+              startTimer();
           }
-          let startTime
+          let startTime;
           function startTimer(){
-              timerElement.innerText = 0
-              startTime=new Date()
+              timerElement.innerText = 0;
+              startTime=new Date();
               setInterval(() =>{
-                timerElement.innerText = getTimerTime()
-              },1000)
+                timerElement.innerText = getTimerTime();
+              },1000);
           }
           function getTimerTime() {
-            return Math.floor((new Date() - startTime) / 1000)
+            return Math.floor((new Date() - startTime) / 1000);
           }
         
-          renderNewQuote()
+          renderNewQuote();
